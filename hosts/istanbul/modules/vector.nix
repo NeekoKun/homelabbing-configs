c5be4797871ef48b48b4dd2aa1b0c206eb7a1f34
Leonardo Ricci Mingani <leonardo.riccimingani@gmail.com>
2025-12-18 17:30:12 +0100

    Testing Grafana

--- hosts/istanbul/modules/vector.nix
+++ hosts/istanbul/modules/vector.nix
@@ -25,7 +25,7 @@ in
 
         host_metrics = {
           type = "host_metrics";
-          collectors = [ "cpu" "memory" "network" ];
+          collectors = [ "cpu" "memory" "network" "filesystem" ];
           scrape_interval_secs = 15;
         };
       };
