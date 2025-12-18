dbd4f1fdb3203b368a49ecf32b0f8897460066ea
NeekoKun <neekokun@proton.me>
2025-12-18 17:29:20 +0100

    Fixed Istanbul Vector config

--- hosts/istanbul/modules/vector.nix
+++ hosts/istanbul/modules/vector.nix
@@ -1,4 +1,4 @@
-{ vars, ... }:
+{ config, vars, ... }:
 
 let
   net = vars.network;
@@ -7,43 +7,114 @@ in
   services.vector = {
     enable = true;
 
+    journaldAccess = true;
+
     settings = {
+      api = {
+        enabled = true;
+        address = "127.0.0.1:8686";
+      };
+
       sources = {
         journald = {
           type = "journald";
-        };
-
-        suricata = {
-          type = "file";
-          include = [ "/var/log/suricata/eve.json" ];
+          exclude_matches = {
+            _SYSTEMD_UNIT = [ "vector.service" ];
+          };
         };
 
         host_metrics = {
           type = "host_metrics";
-          collectors = [ "cpu" "memory" "network" "disk" ];
+          collectors = [ "cpu" "memory" "network" ];
+          scrape_interval_secs = 15;
         };
       };
 
       transforms = {
+        add_hostname = {
+          type = "remap";
+          inputs = [ "host_metrics" ];
+          source = ''
+            .host = "${config.networking.hostName}"
+          '';
+        };
+
         add_metadata = {
           type = "remap";
-          inputs = [ "journald" "suricata" ];
+          inputs = [ "journald" ];
           source = ''
-            .host = "${net.internal.istanbul}"
-            .role = "gateway"
+            .job = "vector"
+            .host = "${config.networking.hostName}"
+
+            if exists(._SYSTEMD_UNIT) {
+              .unit = string!(._SYSTEMD_UNIT)
+            }
+
+            if exists(.MESSAGE) {
+              .message = string!(.MESSAGE)
+            }
           '';
         };
       };
 
       sinks = {
+        prometheus = {
+          type = "prometheus_remote_write";
+          inputs = [ "add_hostname" ];
+          endpoint = "http://${net.internal.rome}:${toString vars.services.prometheus.http_port}/api/v1/write";
+
+          healthcheck.enabled = true;
+
+          batch = {
+            max_events = 1000;
+            timeout_secs = 1;
+          };
+
+          request = {
+            retry_attempts = 5;
+            timeout_secs = 60;
+          };
+        };
+
         loki = {
           type = "loki";
           inputs = [ "add_metadata" ];
           endpoint = "http://${net.internal.rome}:${toString vars.services.loki.http_port}";
           encoding.codec = "json";
-          labels.host = "{{ host }}";
+
+          labels = {
+            job = "{{ job }}";
+            host = "{{ host }}";
+          };
+
+          remove_label_fields = true;
+
+          batch = {
+            max_bytes = 1048576;
+            timeout_secs = 1;
+          };
+
+          out_of_order_action = "accept";
+        };
+
+        debug_metrics = {
+          type = "console";
+          inputs = [ "add_hostname" ];
+          encoding.codec = "json";
+          target = "stdout";
         };
       };
+    };
+  };
+
+  systemd.services.vector = {
+    environment = {
+      VECTOR_LOG = "warn";
+    };
+
+    serviceConfig = {
+      LogRateLimitIntervalSec = 0;
+      LogRateLimitBurst = 0;
     };
   };
 }
