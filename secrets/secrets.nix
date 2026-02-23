3fb8c7a8676cc2a847f775b1ee86b94c8f385d99
NeekoKun <neekokun@proton.me>
2026-02-23 17:33:14 +0100

    Admin password reencrypted for Thebes

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,7 +3,7 @@ let
   
   istanbul = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOCBrszJ63yn00uV3kTjNXp9EfezBLXCD8agN4neVUAz root@istanbul";
   rome     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC0yYswP+ivHjRZmM25CIhgN0AsDz+qJfRzuQdPZ6Q5Q root@rome";
-  thebes   = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKMfAhHjpmSY6eTydJtNfMJyM9vRY5nceuY0yv0Xx8+a root@thebes";
+  thebes   = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDeTfqJu6q8Wo13Cp4t7u5Mpr+u9ou8RcOV3lsEDqn45 root@thebes";
 
   allHosts = [ admin istanbul rome thebes ];
 in
