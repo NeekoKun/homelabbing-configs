a7eff7677366b69930ff1e0e491f0984c9b12f52
NeekoKun <neekokun@proton.me>
2026-02-24 10:20:07 +0100

    Synapse login shenanigans

--- hosts/babylon/modules/synapse.nix
+++ hosts/babylon/modules/synapse.nix
@@ -4,8 +4,14 @@ let
 
 in
 {
+  age.secrets.synapseSecret = {
+    file = "${flakeRoot}/secrets/synapse-secret.age";
+    owner = "matrix-synapse";
+  };
+
   services.matrix-synapse = {
     enable = true;
+    #extraConfigFiles = [ config.age.secrets.synapseSecret.path ];
     settings = {
       server_name = "${vars.network.DNS.domain}.${vars.network.DNS.tld}";
       public_baseurl = "https://matrix.${vars.network.DNS.domain}.${vars.network.DNS.tld}";
@@ -26,11 +32,9 @@ in
         }
       ];
 
-      age.secrets.synapseSecret.file = "${flakeRoot}/secrets/synapse-secret.age";
-
       enable_registration = true;
       registration_requires_token = true;
-      registration_shared_secret = age.secrets.synapseSecret.content;
+      registration_shared_secret = "passwordMoltoSicura"; # TODO: age-encrypt
     };
   };
 
