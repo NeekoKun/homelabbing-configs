b479e9e24fa7e85bf40086ff1affce9658f0687c
Leonardo Ricci Mingani <leonardo.riccimingani@gmail.com>
2026-02-27 16:26:08 +0100

    Trusted domains for debugging

--- hosts/alexandria/modules/nextcloud.nix
+++ hosts/alexandria/modules/nextcloud.nix
@@ -6,6 +6,7 @@
   services.nextcloud = {
     enable = true;
     hostName = "nextcloud.${vars.network.DNS.domain}.${vars.network.DNS.tld}";
+    trusted_domains = [ vars.network.internal.istanbul vars.network.internal.alexandria ];
     config = {
       adminpassFile = config.age.secrets.nextcloudAdminPassword.path;
       dbtype = "sqlite";
@@ -16,6 +17,4 @@
       trusted_proxies = [ "${vars.network.internal.istanbul}" ];
     };
   };
-
-  services.nginx.enable = true;
 }
