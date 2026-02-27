787ab21938edf870b7c40a709209448a3ab1f6f0
Leonardo Ricci Mingani <leonardo.riccimingani@gmail.com>
2026-02-27 17:29:50 +0100

    Removing duplicate headers

--- hosts/istanbul/modules/nginx.nix
+++ hosts/istanbul/modules/nginx.nix
@@ -24,7 +24,7 @@ in
       locations."/" = {
         proxyPass = "http://${vars.network.internal.alexandria}";
         extraConfig = ''
-          proxy_set_header Host $host;
+          proxy_http_version 1.1;
           proxy_set_header X-Real-IP $remote_addr;
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
           proxy_set_header X-Forwarded-Proto $scheme;
