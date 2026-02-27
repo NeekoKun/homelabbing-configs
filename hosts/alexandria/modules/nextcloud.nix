81a0a300191d418d9b1de9122bb889b298a04208
Leonardo Ricci Mingani <leonardo.riccimingani@gmail.com>
2026-02-27 16:28:11 +0100

    Wrong placement

--- hosts/alexandria/modules/nextcloud.nix
+++ hosts/alexandria/modules/nextcloud.nix
@@ -6,7 +6,6 @@
   services.nextcloud = {
     enable = true;
     hostName = "nextcloud.${vars.network.DNS.domain}.${vars.network.DNS.tld}";
-    trusted_domains = [ vars.network.internal.istanbul vars.network.internal.alexandria ];
     config = {
       adminpassFile = config.age.secrets.nextcloudAdminPassword.path;
       dbtype = "sqlite";
@@ -14,6 +13,7 @@
 
     settings = {
       overwriteprotocol = "https";
+      trusted_domains = [ vars.network.internal.istanbul vars.network.internal.alexandria ];
       trusted_proxies = [ "${vars.network.internal.istanbul}" ];
     };
   };
