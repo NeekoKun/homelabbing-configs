7ec9ed3db4987d9524194c33760c103dd18d0227
Leonardo Ricci Mingani <leonardo.riccimingani@gmail.com>
2026-02-27 17:03:20 +0100

    Copilot help me please

--- hosts/alexandria/modules/nextcloud.nix
+++ hosts/alexandria/modules/nextcloud.nix
@@ -1,5 +1,8 @@
 { config, vars, flakeRoot, pkgs, ... }:
 
+let
+  net = vars.network;
+in
 {
   age.secrets.nextcloudAdminPassword.file = "${flakeRoot}/secrets/nextcloud-admin-password.age";
   
@@ -8,6 +11,7 @@
   services.nextcloud = {
     enable = true;
     hostName = "nextcloud.${vars.network.DNS.domain}.${vars.network.DNS.tld}";
+    https = false;
     config = {
       adminpassFile = config.age.secrets.nextcloudAdminPassword.path;
       dbtype = "sqlite";
@@ -15,6 +19,7 @@
 
     settings = {
       overwriteprotocol = "https";
+      overwritehost = "nextcloud.${vars.network.DNS.domain}.${vars.network.DNS.tld}";
       trusted_domains = [
         vars.network.internal.istanbul
         vars.network.internal.alexandria
@@ -22,5 +27,10 @@
       ];
       trusted_proxies = [ vars.network.internal.istanbul ];
     };
+  };
+
+  services.nginx.virtualHosts."nextcloud.${net.DNS.domain}.${net.DNS.tld}" = {
+    listen = [{ addr = net.internal.alexandria; port = 80; }];
+    forceSSL = false;
   };
 }
