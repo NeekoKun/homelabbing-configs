027f99844b3b4226502685be5811b74e5b6a2af6
NeekoKun <neekokun@proton.me>
2026-05-29 11:51:19 +0200

    Added screen "sleep" after 60s

--- configuration.nix
+++ configuration.nix
@@ -57,6 +57,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelParams = [ "consoleblank=60" ];
 
   # Set your time zone.
   time.timeZone = "Europe/Rome";
